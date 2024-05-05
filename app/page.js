9b5b75edfaa273d71af50b0cb06e43dc92afffc6
oliullah05 <mdolihasan@gmail.com>
2024-05-05 15:39:43 +0600

    detect latitude longitude using browser api navigator.geolocation.getCurrentPosition

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
+import LocationDetector from "@/components/LocationDetector";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>Home Page</div>
+   <LocationDetector></LocationDetector>
   );
 }
---
+++ components/LocationDetector.jsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const LocationDetector = () => {
+  const [loading, setLoading] = useState(false);
+  const searchPerams = useSearchParams();
+  const pathname = usePathname()
+  const router = useRouter()
+
+  useEffect(() => {
+    setLoading(true)
+    const params = new URLSearchParams(searchPerams)
+
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        params.set("latitude", position.coords.latitude)
+        params.set("longitude", position.coords.longitude)
+        setLoading(false)
+        router.push(`/current/${params.toString()}`)
+      })
+    }
+  }, [pathname,searchPerams])
+  return (
+    <div>
+      LocationDetector
+    </div>
+  );
+};
+
+export default LocationDetector;
